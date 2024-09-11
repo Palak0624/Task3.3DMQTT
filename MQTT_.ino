be961a6f5b5fd734646e53733cc6b3e22dba0c79
Palak0624 <palak4832.be23@chitkara.edu.in>
2024-09-11 20:18:11 +0530

    Update MQTT_.ino

--- MQTT_.ino
+++ MQTT_.ino
@@ -1,106 +1,147 @@
 #include <WiFiNINA.h>
-
 #include <PubSubClient.h>
 
-const char* ssid = "Palak";  
-const char* password = "11223344"; 
-const char* mqtt_server = "broker.emqx.io";
-const int mqtt_port = 1883;
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-const int trigPin = 11;
+// Constants
+const char* wifiSSID = "Palak";  
+const char* wifiPassword = "11223344"; 
+const char* mqttBroker = "broker.emqx.io";
+const int mqttBrokerPort = 1883;
+const int triggerPin = 11;
 const int echoPin = 12;
 const int ledPin = 2;
-long duration;
-int distance;
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-
-  setup_wifi();
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
-
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.subscribe("SIT210/wave");
-}
-
-void setup_wifi() {
-  delay(10);
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect("ArduinoClient")) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
+class WiFiManager {
+public:
+    void connect(const char* ssid, const char* password) {
+        WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            Serial.print(".");
+        }
+        Serial.println("");
+        Serial.println("WiFi connected");
     }
-  }
-}
+};
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
+class MQTTManager {
+private:
+    PubSubClient client;
+public:
+    MQTTManager(WiFiClient& wifiClient) : client(wifiClient) {}
+
+    void setup(const char* broker, int port) {
+        client.setServer(broker, port);
+    }
+
+    void reconnect() {
+        while (!client.connected()) {
+            Serial.print("Connecting to MQTT...");
+            if (client.connect("ArduinoClient")) {
+                Serial.println("connected");
+            } else {
+                Serial.print("failed, rc=");
+                Serial.print(client.state());
+                Serial.println(" try again in 5 seconds");
+                delay(5000);
+            }
+        }
+    }
+
+    void setCallback(MQTT_CALLBACK_SIGNATURE) {
+        client.setCallback(callback);
+    }
+
+    void loop() {
+        client.loop();
+    }
+
+    void publish(const char* topic, const char* message) {
+        client.publish(topic, message);
+    }
+
+    bool connected() {
+        return client.connected();
+    }
+};
+
+class DistanceSensor 
+{
+  private:
+    int triggerPin;
+    int echoPin;
+    int ledPin;
+  public:
+  DistanceSensor(int trigPin, int echoPin, int ledPin)
+        : triggerPin(trigPin), echoPin(echoPin), ledPin(ledPin) {
+        pinMode(triggerPin, OUTPUT);
+        pinMode(echoPin, INPUT);
+        pinMode(ledPin, OUTPUT);
+    }
+
+    long measureDistance() {
+        digitalWrite(triggerPin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(triggerPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(triggerPin, LOW);
+
+        long duration = pulseIn(echoPin, HIGH);
+        return duration * 0.034 / 2;
+    }
+
+    void flashLED(int times) 
+    {
+        for (int i = 0; i < times; i++) 
+        {
+            digitalWrite(ledPin, HIGH);
+            delay(500);
+            digitalWrite(ledPin, LOW);
+            delay(500);
+        }
+    }
+};
+
+// Global instances
+WiFiClient wifiClient;
+MQTTManager mqttManager(wifiClient);
+DistanceSensor distanceSensor(triggerPin, echoPin, ledPin);
+
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  Serial.print("Message received on [");
   Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++) 
+  {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
-  // Flash LED 3 times
-  for (int i = 0; i < 100; i++) {
-    digitalWrite(ledPin, HIGH);
-    delay(500);
-    digitalWrite(ledPin, LOW);
-    delay(500);
-  }
+  distanceSensor.flashLED(3); 
 }
 
-void loop() {
-  if (!client.connected()) {
-    reconnect();
+void setup() 
+{
+  Serial.begin(115200);
+  WiFiManager wifiManager;
+  wifiManager.connect(wifiSSID, wifiPassword);
+  mqttManager.setup(mqttBroker, mqttBrokerPort);
+  mqttManager.setCallback(callback);
+  mqttManager.reconnect();
+  mqttManager.publish("SIT210/wave", "Setup complete");
+}
+
+void loop() 
+{
+  if (!mqttManager.connected()) 
+  {
+    mqttManager.reconnect();
   }
-  client.loop();
+  mqttManager.loop();
 
-  // Measure the distance using the ultrasonic sensor
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
-
-  if (distance < 10) {  
-    String message = "Palak";  
-    client.publish("SIT210/wave", message.c_str());
-    delay(2000);  
+  long distance = distanceSensor.measureDistance();
+  if (distance < 10) 
+  {
+    mqttManager.publish("SIT210/wave", "Palak");
+    delay(2000);
   }
-
 }
